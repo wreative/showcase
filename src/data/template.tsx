f90b4ded44dccac2c17a81009db2c51b93e8e3c7
Moh Ravi Dwi Putra <ravidwiputra@gmail.com>
2025-07-10 23:48:01 +0700

    Update data

--- src/data/template.tsx
+++ src/data/template.tsx
@@ -1,6 +1,10 @@
 import fajarFloristCover from '../../assets/cover/fajar-florist.webp';
 import waseBumiIndonesiaCover from '../../assets/cover/wase-bumi-indonesia.webp';
 import firstMediaSurabayaCover from '../../assets/cover/first-media-surabaya.webp';
+import kontraktorSurabayaCover from '../../assets/cover/kontraktor-surabaya.webp';
+import wreativeCover from '../../assets/cover/wreative.webp';
+import wreativeStoreCover from '../../assets/cover/wreative-store.webp';
+import cubicleToiletCover from '../../assets/cover/cubicle-toilet.webp';
 
 export interface TemplateData {
     id: number;
@@ -15,21 +19,21 @@ export const templates: TemplateData[] = [
         id: 1,
         title: "Kontraktor Surabaya",
         category: "Services",
-        image: "https://images.unsplash.com/photo-1551288049-bebda4e38f71?q=80&w=2000&auto=format&fit=crop",
-        url: "http://konstraktorsurabaya.com/"
+        image: kontraktorSurabayaCover,
+        url: "https://kontraktorsurabaya.wreative.com/"
     },
     {
         id: 2,
         title: "Wase Bumi Indonesia",
         category: "Business",
         image: waseBumiIndonesiaCover,
-        url: "https://wasebumiindonesia.com/"
+        url: "https://wasebumiindonesia.wreative.com/"
     },
     {
         id: 3,
         title: "Cubicle Toilet",
         category: "Services",
-        image: "https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?q=80&w=2000&auto=format&fit=crop",
+        image: cubicleToiletCover,
         url: "https://cubicletoiletid.com/"
     },
     {
@@ -43,7 +47,7 @@ export const templates: TemplateData[] = [
         id: 5,
         title: "Wreative Store",
         category: "E-Commerce",
-        image: "https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=2000&auto=format&fit=crop",
+        image: wreativeStoreCover,
         url: "https://store.wreative.com"
     },
     {
@@ -57,7 +61,7 @@ export const templates: TemplateData[] = [
         id: 7,
         title: "Wreative",
         category: "Business",
-        image: "https://images.unsplash.com/photo-1472851294608-062f824d29cc?q=80&w=2000&auto=format&fit=crop",
+        image: wreativeCover,
         url: "https://wreative.com/"
     },
     {
@@ -80,5 +84,47 @@ export const templates: TemplateData[] = [
         category: "Business",
         image: firstMediaSurabayaCover,
         url: "https://firstmediasurabaya.wreative.com/"
+    },
+    {
+        id: 11,
+        title: "Wahyu Dewanagari",
+        category: "Business",
+        image: "https://images.unsplash.com/photo-1499750310107-5fef28a66643?q=80&w=2000&auto=format&fit=crop",
+        url: "https://wreative.com"
+    },
+    {
+        id: 12,
+        title: "Toilet Portable",
+        category: "Services",
+        image: "https://images.unsplash.com/photo-1499750310107-5fef28a66643?q=80&w=2000&auto=format&fit=crop",
+        url: "https://wreative.com"
+    },
+    {
+        id: 13,
+        title: "Sakpattana Jawa Timur",
+        category: "Business",
+        image: "https://images.unsplash.com/photo-1499750310107-5fef28a66643?q=80&w=2000&auto=format&fit=crop",
+        url: "https://wreative.com"
+    },
+    {
+        id: 14,
+        title: "Pos Satpam Surabaya",
+        category: "Services",
+        image: "https://images.unsplash.com/photo-1499750310107-5fef28a66643?q=80&w=2000&auto=format&fit=crop",
+        url: "https://wreative.com"
+    },
+    {
+        id: 15,
+        title: "Modern Coco International",
+        category: "Business",
+        image: "https://images.unsplash.com/photo-1499750310107-5fef28a66643?q=80&w=2000&auto=format&fit=crop",
+        url: "https://wreative.com"
+    },
+    {
+        id: 16,
+        title: "CV Putra Kubota",
+        category: "Business",
+        image: "https://images.unsplash.com/photo-1499750310107-5fef28a66643?q=80&w=2000&auto=format&fit=crop",
+        url: "https://wreative.com"
     },
 ];
