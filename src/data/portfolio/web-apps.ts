273f2e26b44844754f9e93fed74ffc1f255caab8
Moh Ravi Dwi Putra <ravidwiputra@gmail.com>
2026-09-15 10:31:20 +0700

    feat: update web app portfolio tags to reflect new technology stack

--- src/data/portfolio/web-apps.ts
+++ src/data/portfolio/web-apps.ts
@@ -36,7 +36,7 @@ export const webAppPortfolios: Omit<PortfolioItem, 'id' | 'slug'>[] = [
       'Application for women-only Pilates studio. This app allows users to book classes, track progress, and receive updates on schedules and promotions. Additional features include social media integration, fitness tracking, and personalized content to enhance the user experience.',
     descriptionId:
       'Aplikasi Studio Pilates Khusus Wanita. Aplikasi ini memungkinkan pengguna untuk memesan kelas, melacak kemajuan, dan menerima pembaruan tentang jadwal dan promosi. Fitur tambahan termasuk integrasi media sosial, pelacakan kebugaran, dan konten yang dipersonalisasi untuk meningkatkan pengalaman pengguna.',
-    tags: ['React Native', 'Expo', 'Firebase', 'Push Notifications'],
+    tags: ['Laravel', 'Livewire', 'Tailwind CSS', 'Alpine.js', 'MySQL'],
     gallery: wreativeAppGallery,
   }),
   entry({
@@ -49,7 +49,17 @@ export const webAppPortfolios: Omit<PortfolioItem, 'id' | 'slug'>[] = [
       'Event and Ticketing Application. This app allows users to purchase event tickets, manage schedules, and receive updates on events they are interested in.',
     descriptionId:
       'Aplikasi Event (Kegiatan) dan Tiket. Aplikasi ini memungkinkan pengguna untuk membeli tiket acara, mengelola jadwal, dan menerima pembaruan tentang acara yang mereka minati.',
-    tags: ['Flutter', 'Dart', 'Firebase', 'Google Maps API', 'Midtrans'],
+    tags: [
+      'Laravel',
+      'Livewire',
+      'Tailwind CSS',
+      'Alpine.js',
+      'MySQL',
+      'FilamentPHP',
+      'Midtrans',
+      'Sync System',
+      'Sentry',
+    ],
     gallery: foodAppGallery,
   }),
   entry({
@@ -62,7 +72,17 @@ export const webAppPortfolios: Omit<PortfolioItem, 'id' | 'slug'>[] = [
       'All-in-One Application Service. This app allows users to access various premium applications within the Gabungin service or Workspace.',
     descriptionId:
       'Layanan Aplikasi yang menggabungkan semua aplikasi menjadi satu. Aplikasi ini memungkinkan pengguna untuk mengakses berbagai aplikasi premium yang ada di dalam layanan atau Workspace Gabungin.',
-    tags: ['Flutter', 'Dart', 'Firebase', 'Google Maps API', 'Midtrans'],
+    tags: [
+      'Next.js',
+      'Tailwind CSS',
+      'TypeScript',
+      'MySQL',
+      'TriPay',
+      'Base UI',
+      'Browser Extension',
+      'Framer Motion',
+      'Sentry',
+    ],
     gallery: foodAppGallery,
   }),
   entry({
@@ -75,7 +95,17 @@ export const webAppPortfolios: Omit<PortfolioItem, 'id' | 'slug'>[] = [
       'Game Top Up and Digital Products Service. This app allows users to top up games and purchase various other digital products.',
     descriptionId:
       'Layanan Top Up Game dan Produk Digital. Aplikasi ini memungkinkan pengguna untuk melakukan top up game dan membeli berbagai produk digital lainnya.',
-    tags: ['Flutter', 'Dart', 'Firebase', 'Google Maps API', 'Midtrans'],
+    tags: [
+      'Next.js',
+      'Tailwind CSS',
+      'TypeScript',
+      'MySQL',
+      'Midtrans',
+      'Framer Motion',
+      'PostHog',
+      'TurboPack',
+      'Sentry',
+    ],
     gallery: foodAppGallery,
   }),
   entry({
@@ -88,7 +118,7 @@ export const webAppPortfolios: Omit<PortfolioItem, 'id' | 'slug'>[] = [
       'Infrastructure Information System Application. This app is designed to provide information regarding the development of infrastructure in a specific area. It offers solutions for managing and monitoring digital infrastructure.',
     descriptionId:
       'Aplikasi yang dibentuk dengan tujuan memberikan informasi mengenai perkembangan pembangunan infrastruktur di wilayah tertentu. Aplikasi ini menyediakan solusi untuk mengelola dan memantau infrastruktur digital.',
-    tags: ['Flutter', 'Dart', 'Firebase', 'Google Maps API', 'Midtrans'],
+    tags: ['Laravel', 'CSS', 'JavaScript', 'MySQL'],
     gallery: foodAppGallery,
   }),
   entry({
@@ -101,7 +131,7 @@ export const webAppPortfolios: Omit<PortfolioItem, 'id' | 'slug'>[] = [
       'Public Private Partnership Data Room. This app is designed to provide a centralized platform for managing and sharing data related to public-private partnership projects or PJPK.',
     descriptionId:
       'Ruangan Data untuk Kerjasama Publik-Privat. Aplikasi ini dirancang untuk menyediakan platform terpusat dalam mengelola dan berbagi data yang terkait dengan proyek kerjasama publik-privat atau Penanggung Jawab Proyek Kerja Sama (PJPK).',
-    tags: ['Flutter', 'Dart', 'Firebase', 'Google Maps API', 'Midtrans'],
+    tags: ['Laravel', 'CSS', 'JavaScript', 'MySQL'],
     gallery: foodAppGallery,
   }),
   entry({
@@ -114,7 +144,7 @@ export const webAppPortfolios: Omit<PortfolioItem, 'id' | 'slug'>[] = [
       'Sajodo - Snack & Food. This app is designed to provide a platform for managing and sharing data related to snack and food products.',
     descriptionId:
       'Sajodo - Snack & Food. Aplikasi ini dirancang untuk menyediakan platform dalam mengelola dan berbagi data yang terkait dengan produk makanan ringan dan makanan.',
-    tags: ['Flutter', 'Dart', 'Firebase', 'Google Maps API', 'Midtrans'],
+    tags: ['Laravel', 'CSS', 'JavaScript', 'MySQL'],
     gallery: foodAppGallery,
   }),
   entry({
@@ -127,7 +157,7 @@ export const webAppPortfolios: Omit<PortfolioItem, 'id' | 'slug'>[] = [
       'Andromart - Online Store & E-Commerce. This app is designed to provide a platform for managing and sharing data related to online shopping and e-commerce. Additionally, the app also provides features to facilitate the transaction process and product management for sellers.',
     descriptionId:
       'Aplikasi Toko Online & E-Commerce. Aplikasi ini dirancang untuk menyediakan platform dalam mengelola dan berbagi data yang terkait dengan belanja online dan e-commerce. Selain itu, aplikasi ini juga menyediakan fitur untuk mempermudah proses transaksi dan pengelolaan produk bagi para penjual.',
-    tags: ['Flutter', 'Dart', 'Firebase', 'Google Maps API', 'Midtrans'],
+    tags: ['Laravel', 'CSS', 'JavaScript', 'MySQL', 'Datatables'],
     gallery: foodAppGallery,
   }),
 ];
