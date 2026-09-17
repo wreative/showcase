4ab7f534a625232bd77d474f625d79260c1126fa
Moh Ravi Dwi Putra <ravidwiputra@gmail.com>
2026-09-17 17:59:39 +0700

    feat: integrate Lottie animations in TemplateCard and update portfolio data

--- src/data/portfolio/websites.ts
+++ src/data/portfolio/websites.ts
@@ -9,7 +9,7 @@ const wreativeGallery = [
   // img('/assets/cover/wreative-store.webp'),
   // img('/assets/cover/home-baraka.webp'),
   // img('/assets/cover/aviso.webp'),
-  lottie('/assets/lottie/ccd.json', { loop: true, autoplay: true }),
+  lottie('/assets/lottie/55.json', { loop: true, autoplay: true }),
   vid(demoVideo, '/assets/cover/wreative.webp'),
 ];
 
